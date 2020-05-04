b474a6cd70d4e25dcc15efae67371355fb392b28
Alec Tecson <alec.tecson@nipa.ph>
2020-05-04 17:59:10 +0800

    Creating an API Resource

---
+++ Controllers/Resources/ContactResource.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace vega.Controllers.Resources
+{
+    public class ContactResource
+    {
+        [Required]
+        [StringLength(255)]
+        public string ContactName { get; set; }
+        [Required]
+        [StringLength(255)]
+        public string ContactEmail { get; set; }
+        [Required]
+        [StringLength(255)]
+        public string ContactPhone { get; set; }
+    }
+}
