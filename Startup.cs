0c7f8fb980b6955184116d8b4ba757c2900e1940
Alec Tecson <alec.tecson@nipa.ph>
2020-05-05 20:31:00 +0800

    Dependency Inversion Principle

--- Startup.cs
+++ Startup.cs
@@ -8,6 +8,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using vega.Core;
 using vega.Persistence;
 
 namespace vega
