c42a3c62a19b0bf8df0b9ff77ad1ec18c0846f46
Alec Tecson <alec.tecson@nipa.ph>
2020-05-05 15:26:18 +0800

    Mapping Collections

--- Mapping/MappingProfile.cs
+++ Mapping/MappingProfile.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using System.Linq;
 using AutoMapper;
 using vega.Controllers.Resources;
@@ -23,7 +24,28 @@ namespace vega.Mapping
                 .ForMember(v => v.ContactName, opt => opt.MapFrom(vr => vr.Contact.Name))
                 .ForMember(v => v.ContactEmail, opt => opt.MapFrom(vr => vr.Contact.Email))
                 .ForMember(v => v.ContactPhone, opt => opt.MapFrom(vr => vr.Contact.Phone))
-                .ForMember(v => v.Features, opt => opt.MapFrom(vr => vr.Features.Select(id => new VehicleFeature { FeatureId = id })));
+                .ForMember(v => v.Features, opt => opt.Ignore())
+                .AfterMap((vr, v) => {
+                    var removedFeatures = new List<VehicleFeature>();
+
+                    foreach(var feature in v.Features) 
+                    {
+                        if (vr.Features.Contains(feature.FeatureId)) 
+                            v.Features.Remove(feature);
+                    }
+
+                    foreach(var feature in removedFeatures)
+                    {
+                        v.Features.Remove(feature);
+                    }
+
+                    // Add new features
+                    foreach (var id in vr.Features)
+                    {
+                        if(v.Features.Any(f => f.FeatureId == id))
+                            v.Features.Add(new VehicleFeature { FeatureId = id });
+                    }
+                });
         }
     }
 }
