2a58280940dfb6a416c7a238424739994743807f
Alec Tecson <alec.tecson@nipa.ph>
2020-05-05 19:29:08 +0800

    Decoupling from Entity Framework

--- Persistence/IVehicleRepository.cs
+++ Persistence/IVehicleRepository.cs
@@ -5,6 +5,8 @@ namespace vega.Persistence
 {
     public interface IVehicleRepository
     {
-         public Task<Vehicle> GetVehicle(int id);
+         public Task<Vehicle> GetVehicle(int id, bool includeRelated = true);
+         public void Add(Vehicle vehicle);
+         public void Remove(Vehicle vehicle);
     }
 }
