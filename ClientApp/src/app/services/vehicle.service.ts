aed0f59249fd46a2ada3a15dd02a01ee007e4369
Alec Tecson <alec.tecson@nipa.ph>
2020-05-07 18:10:41 +0800

    Submitting the Form

--- ClientApp/src/app/services/vehicle.service.ts
+++ ClientApp/src/app/services/vehicle.service.ts
@@ -20,4 +20,10 @@ export class VehicleService {
       map(response => response)
     );
   }
+
+  create(vehicle) {
+    return this.http.post('/api/vehicles', vehicle).pipe(
+      map(response => response)
+    )
+  }
 }
