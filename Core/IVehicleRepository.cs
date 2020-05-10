fa8fe7f29404a70a7db4c7dc5704c2bfa6887c73
Alec Tecson <alec.tecson@nipa.ph>
2020-05-10 20:03:20 +0800

    Filtering on the Server

--- Core/IVehicleRepository.cs
+++ Core/IVehicleRepository.cs
@@ -9,6 +9,6 @@ namespace vega.Core
          public Task<Vehicle> GetVehicle(int id, bool includeRelated = true);
          public void Add(Vehicle vehicle);
          public void Remove(Vehicle vehicle);
-         public Task<IEnumerable<Vehicle>> GetVehicles();
+         public Task<IEnumerable<Vehicle>> GetVehicles(Filter filter);
     }
 }
--- Persistence/VehicleRepository.cs
+++ Persistence/VehicleRepository.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 using vega.Core;
@@ -36,14 +37,19 @@ namespace vega.Persistence
             context.Vehicles.Remove(vehicle);
         }
 
-        public async Task<IEnumerable<Vehicle>> GetVehicles()
+        public async Task<IEnumerable<Vehicle>> GetVehicles(Filter filter)
         {
-            return await context.Vehicles
+            var query = context.Vehicles
                 .Include(m => m.Model)
                     .ThenInclude(m => m.Make)
                 .Include(v => v.Features)
                     .ThenInclude(vf => vf.Feature)
-                .ToListAsync();
+                .AsQueryable();
+
+            if (filter.MakeId.HasValue)
+                query = query.Where(v => v.Model.MakeId == filter.MakeId.Value);
+
+            return await query.ToListAsync();
         }
     }
 }
